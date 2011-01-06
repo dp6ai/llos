210361b51e585e8bd683c0c2263e8e434ff4866f
lloydloom <lloydloom@1638c4b9-3cb2-4755-b50b-1a6675a9391c>
2011-01-06 22:30:00 +0000

    Project Capistrano ready for deployment
    git-svn-id: https://llos.svn.beanstalkapp.com/llos@3 1638c4b9-3cb2-4755-b50b-1a6675a9391c

---
+++ trunk/config/deploy.rb
@@ -0,0 +1,34 @@
+
+default_run_options[:pty] = true 
+
+set :application, "llos"
+set :repository,  "http://llos.svn.beanstalkapp.com/llos/trunk"
+set :scm_username , "lloydloom"
+set :scm_password, "L10yd100m"
+# set :scm_command, "/usr/bin/svn"
+set :deploy_to, "~/#{application}"
+set :user, "deployment"
+set :password, "d3pl0ym3nt7584"
+
+set :scm, :subversion
+set :keep_releases, 3      unless exists?(:keep_releases)
+
+role :web, "www.lloydloom.com"                          # Your HTTP server, Apache/etc
+role :app, "www.lloydloom.com"                          # This may be the same as your `Web` server
+role :db,  "www.lloydloom.com", :primary => true # This is where Rails migrations will run
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end    
+
+
+after :deploy, "deploy:restart"
+after :deploy, "deploy:cleanup"
