b4ca67eed1ec9456c5f9d2122920deaaded0dd9c
lloydloom <lloydloom@1638c4b9-3cb2-4755-b50b-1a6675a9391c>
2011-01-12 13:32:11 +0000

    - Clearer error messages upon contact form submission
    git-svn-id: https://llos.svn.beanstalkapp.com/llos@15 1638c4b9-3cb2-4755-b50b-1a6675a9391c

--- trunk/public/javascripts/application.js
+++ trunk/public/javascripts/application.js
@@ -1,8 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 jQuery(document).ready(function(){
-  // only care about ranges for now
-  if (jQuery("li[id="+LLOS.current_range+"]").length > 0) {
-    jQuery('ul[id=ranges]').scrollTo('li[id='+LLOS.current_range+']', 500, { easing:'swing' });
-  }
+    // only care about ranges for now
+    if (jQuery("li[id="+LLOS.current_range+"]").length > 0) {
+      jQuery('ul[id=ranges]').scrollTo('li[id='+LLOS.current_range+']', 500, { easing:'swing' });
+    }
+
+    if (jQuery("#errorExplanation").length > 0) {
+        jQuery("#trigger_dialog").fancybox({
+	            'scrolling'		: 'no',
+	            'titleShow'		: false
+        });
+
+        jQuery("#trigger_dialog").trigger('click');
+    }
+
 });
