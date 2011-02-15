efe1836ea8641c47a23cfebcd04f3602fb2578a6
lloydloom <lloydloom@1638c4b9-3cb2-4755-b50b-1a6675a9391c>
2011-02-15 13:27:35 +0000

    - Title to contact forms - Bug in Heritage menu highlighting
    git-svn-id: https://llos.svn.beanstalkapp.com/llos@39 1638c4b9-3cb2-4755-b50b-1a6675a9391c

--- trunk/app/models/notifier.rb
+++ trunk/app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
   def contact(message)
-#    recipients "info@lloydloom.com, lloswebsite@googlemail.com, henryharris@hotmail.co.uk"
-    recipients "matt.spendlove@gmail.com"
+    recipients "info@lloydloom.com, lloswebsite@googlemail.com, henryharris@hotmail.co.uk"
+#    recipients "matt.spendlove@gmail.com"
 
     from       message.email
     subject    message.subject
